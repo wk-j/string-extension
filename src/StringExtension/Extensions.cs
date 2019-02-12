9defccf565062d5a7e876a19751a1c5433b9b2e6
wk-j <somnuk.wk@gmail.com>
2019-02-12 18:22:16 +0700

    ToDefaultIfNullOrEmpty

--- src/StringExtension/Extensions.cs
+++ src/StringExtension/Extensions.cs
@@ -2,8 +2,12 @@
 
 namespace StringExtension {
     public static class Extensions {
-        public static string ToEmptyIfNull(this string str) {
-            return string.IsNullOrEmpty(str) ? string.Empty : str;
-        }
+
+        public static string ToEmptyIfNull(this string str) =>
+            string.IsNullOrEmpty(str) ? string.Empty : str;
+
+        public static string ToDefaultIfNullOrEmpty(this string str, string value) =>
+            string.IsNullOrEmpty(str) ? value : str;
+
     }
 }
